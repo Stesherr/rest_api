2887c11d0ac0bd1c1c1ab869b50331a10813f0df
Stesherr <stesherr@espol.edu.ec>
2023-12-12 23:23:30 -0500

    Correcion del sequelize y mysql2, ademas de completar los controladores

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -8,16 +8,69 @@ router.get('/findAll', function(req, res, next) {
     res.send("GET All")
 });
 router.get('/findById/:id', function(req, res, next) {
-    res.send("GET By Id")
+    let id = parseInt(req.params.id);
+
+  Suppliers.findOne({  
+      where: { 
+        [Op.and]: [
+          {SupplierID: id}
+        ]
+      }
+  })  
+  .then(data => {  
+      res.json(data);  
+  })  
+  .catch(error => res.status(400).send(error)) 
 });
 router.post('/save', function(req, res, next) { 
-    res.send("POST")
+    let {SupplierName, ContactName, Address, City, PostalCode, Country, Phone} = req.body;
+        
+    Suppliers.create({
+        SupplierName: SupplierName, 
+        ContactName: ContactName, 
+        Address: Address, 
+        City: City, 
+        PostalCode: PostalCode, 
+        Country: Country, 
+        Phone: Phone
+    })
+    .then(data => {  
+      res.json(data);  
+  })  
+  .catch(error => res.status(400).send(error)) 
 });
 router.put('/update/:id', function(req, res, next) { 
-    res.send("PUT")
+    let {id, email, username, password} = req.body;
+        
+    Users.update({
+      email: email,
+      username: username,
+      password: password,
+      logins: 0,
+      last_login: 0
+    },
+    {
+        where: {
+          id: parseInt(id)
+        }
+    })
+    .then(users => {  
+      res.json(users);  
+  })  
+  .catch(error => res.status(400).send(error)) 
 });  
 router.delete('/delete/:id', function(req, res, next) { 
-    res.send("DELETE")
+    let id = parseInt(req.params.id);
+          
+      Users.destroy({
+        where: { 
+          id: id
+        }
+      })
+      .then(users => {  
+      res.json(users);  
+  })  
+  .catch(error => res.status(400).send(error))
 });
 
 module.exports = router;
